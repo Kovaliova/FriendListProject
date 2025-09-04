2eeb3cdf3dc709c3e1e1102c82164bfbdcf7816c
Екатерина <dmitrij@MacBook-Air-Dmitrij.local>
2025-09-04 14:09:17 +0200

    adding test for pagination

--- src/components/Pagination.test.js
+++ src/components/Pagination.test.js
@@ -5,13 +5,13 @@ import Pagination from './Pagination';
 describe('Pagination', () => {
   test('не рендерится если всего 1 страница', () => {
     const { container } = render(
-      <Pagination page={1} total={5} limit={10} onPage={() => {}} />
+      <Pagination page={1} total={4} limit={10} onPage={() => {}} />
     );
     expect(container.firstChild).toBeNull();
   });
 
-  test('рендерит страницы корректно и стрелки', () => {
-    render(<Pagination page={3} total={50} limit={5} onPage={() => {}} />);
+  test('корректный рендер страниц и стрелок', () => {
+    render(<Pagination page={3} total={50} limit={4} onPage={() => {}} />);
 
     expect(screen.getByText('←')).toBeInTheDocument();
     expect(screen.getByText('→')).toBeInTheDocument();
@@ -20,15 +20,14 @@ describe('Pagination', () => {
     expect(screen.getByText('2')).toBeInTheDocument();
     expect(screen.getByText('3')).toHaveClass('active');
     expect(screen.getByText('4')).toBeInTheDocument();
-    expect(screen.getByText('5')).toBeInTheDocument();
-    expect(screen.getByText('10')).toBeInTheDocument();
+    expect(screen.getByText('13')).toBeInTheDocument();
 
     expect(screen.getAllByText('…').length).toBeGreaterThan(0);
   });
 
   test('клики по стрелкам вызывают onPage', () => {
     const onPage = jest.fn();
-    render(<Pagination page={3} total={50} limit={5} onPage={onPage} />);
+    render(<Pagination page={3} total={50} limit={4} onPage={onPage} />);
 
     fireEvent.click(screen.getByText('←'));
     expect(onPage).toHaveBeenCalledWith(2);
@@ -38,18 +37,18 @@ describe('Pagination', () => {
   });
 
   test('кнопки стрелок дизейбл на краях', () => {
-    const { rerender } = render(<Pagination page={1} total={50} limit={5} onPage={() => {}} />);
-    expect(screen.getByText('←')).toBeDisabled();
-    expect(screen.getByText('→')).not.toBeDisabled();
+  const { rerender } = render(<Pagination page={1} total={50} limit={4} onPage={() => {}} />);
+  expect(screen.getByText('←')).toBeDisabled();
+  expect(screen.getByText('→')).not.toBeDisabled();
 
-    rerender(<Pagination page={10} total={50} limit={5} onPage={() => {}} />);
-    expect(screen.getByText('→')).toBeDisabled();
-    expect(screen.getByText('←')).not.toBeDisabled();
-  });
+  rerender(<Pagination page={13} total={50} limit={4} onPage={() => {}} />);
+  expect(screen.getByText('→')).toBeDisabled();
+  expect(screen.getByText('←')).not.toBeDisabled();
+ });
 
   test('клик по номеру страницы вызывает onPage', () => {
     const onPage = jest.fn();
-    render(<Pagination page={1} total={50} limit={5} onPage={onPage} />);
+    render(<Pagination page={1} total={50} limit={4} onPage={onPage} />);
 
     fireEvent.click(screen.getByText('3'));
     expect(onPage).toHaveBeenCalledWith(3);
